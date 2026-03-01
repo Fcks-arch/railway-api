6a064b1eaab855d531e07b3bf7bfc82ab97af389
john <johnpaulbarbado398@gmail.com>
2026-03-01 20:53:54 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?php
+require_once 'cors.php';
+require_once 'config.php';
+// ... rest of your code
+
 // Restaurant API - Entry point
 header('Content-Type: application/json');
 echo json_encode(['status' => 'Restaurant API is running!', 'version' => '1.0']);
