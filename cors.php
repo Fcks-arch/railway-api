3f4a5d4c4741fd3abe5780859284f8b3fed8ff37
john <johnpaulbarbado398@gmail.com>
2026-03-01 21:00:25 +0800

    Add files via upload

---
+++ cors.php
@@ -0,0 +1,12 @@
+<?php
+// CORS Headers - add this to all API requests
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type");
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit;
+}
+?>
